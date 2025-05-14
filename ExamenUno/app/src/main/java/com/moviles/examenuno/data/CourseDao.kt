b6cdf04918bd15ea7b21cf407a023bad5571be66
adrianaOM3 <111209153+adrianaOM3@users.noreply.github.com>
2025-05-14 02:35:08 -0600

    Add course entiy  and repository

--- ExamenUno/app/src/main/java/com/moviles/examenuno/data/CourseDao.kt
+++ ExamenUno/app/src/main/java/com/moviles/examenuno/data/CourseDao.kt
@@ -5,15 +5,16 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.moviles.examenuno.models.Course
+import com.moviles.examenuno.models.CourseEntity
 
 @Dao
 interface CourseDao {
 
     @Query("SELECT * FROM course")
-    suspend fun getAllCourse(): List<Course>
+    suspend fun getAllCourse(): List<CourseEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertCourse(events: List<Course>)
+    suspend fun insertCourse(events: List<CourseEntity>)
 
     @Query("DELETE FROM course")
     suspend fun deleteAllCourse()
--- ExamenUno/app/src/main/java/com/moviles/examenuno/repository/CourseRepository.kt
+++ ExamenUno/app/src/main/java/com/moviles/examenuno/repository/CourseRepository.kt
@@ -1,46 +1,15 @@
 package com.moviles.examenuno.repository
 
+import android.content.Context
+import com.moviles.examenuno.models.Course
+import com.moviles.examenuno.data.DataBaseBuilder
+import com.moviles.examenuno.models.CourseEntity
+
+class CourseRepository(private val context: Context) {
+    private val courseDao = DataBaseBuilder.getInstance(context).courseDao()
 
 
-/*class CourseRepository(private val context: Context) {
 
-    private val courseDao = DatabaseBuilder.getInstance(context).courseDao()
 
-    suspend fun getCourses(): List<Course> {
-        return courseDao.getAllCourses().map { entity ->
-            entity.toDomain()
-        }
-    }
 
-    suspend fun insertCourses(courses: List<Course>) {
-        courseDao.insertCourses(courses.map { course ->
-            course.toEntity()
-        })
-    }
-
-    suspend fun clearCourses() {
-        courseDao.deleteAllCourses()
-    }
-
-    private fun CourseEntity.toDomain(): Course {
-        return Course(
-            id = this.id,
-            name = this.name,
-            description = this.description,
-            image = this.image,
-            schedule = this.schedule,
-            professor = this.professor
-        )
-    }
-
-    private fun Course.toEntity(): CourseEntity {
-        return CourseEntity(
-            id = this.id ?: 0, // Usa 0 si el ID es nulo
-            name = this.name,
-            description = this.description,
-            image = this.image,
-            schedule = this.schedule,
-            professor = this.professor
-        )
-    }
-}*/
+}
